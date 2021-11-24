e57e99b0174eb7a28df4b8414f3c946b26e7031e
sandesh4548 <89025546+sandesh4548@users.noreply.github.com>
2021-11-24 22:01:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// let input = document.getElementsByClassName(".boxb");
+// function cake() {
+//    input.style.display = "";
+// }
+
+function cake() {
+     var x = document.getElementsByClassName(".boxb");
+     if (x.style.display == "block") {
+       x.style.display = "block";
+     } else {
+       x.style.display = "none";
+     }
+   }
